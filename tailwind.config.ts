9c9f336f340f27eb2d0b088aa7d377ace41d8568
Solly <solobarine@gmail.com>
2025-06-02 00:58:24 +0100

    project initialization / dependency config

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "*.{js,ts,jsx,tsx,mdx}",
+  ],
+};
